2788a3805b179e8e1eff80c04b863375d6798f6c
anthony-freitas <92833124+avb-git@users.noreply.github.com>
2021-11-11 08:58:07 -0300

    dfsdfdgdfg

---
+++ execute/test.alarm.py
@@ -0,0 +1,36 @@
+
+##########################################################################################################################
+
+# Imports
+import os
+import sys
+import dotenv
+from py_wapp import Wapp
+
+##########################################################################################################################
+
+# Get Enviromental Variables
+dotenv.load_dotenv()
+
+# Instance Whatsapp
+avbot = Wapp({
+    'address': os.getenv('WHATSAPP_TARGET_ADDRESS'),
+    'user': os.getenv('WHATSAPP_TARGET_USER'),
+    'password': os.getenv('WHATSAPP_TARGET_PASSWORD')
+})
+
+##########################################################################################################################
+#                                                 NTM CASQUILHOS TEMP ALARM                                              #
+##########################################################################################################################
+
+# message
+msg = '*Atenção!* ⚠️ (avbot is running)'
+
+# log
+log = 'iba::test'
+
+# send message
+res = avbot.sends(to='anthony', text=msg, log=log)
+print(res)
+
+##########################################################################################################################
