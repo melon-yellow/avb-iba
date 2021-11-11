0607552dbaa19404ba6d937b4e0828dc69b358de
anthony <78743473+anthony-freitas@users.noreply.github.com>
2021-11-11 14:00:58 -0300

    sadasdasdasd

--- execute/mill/status.alarm/first_fault.py
+++ execute/mill/status.alarm/first_fault.py
@@ -19,7 +19,7 @@ data = json.loads(sys.argv[1])
 # Check Received Data
 cause = prevent_billet.cause(data=data, status='gap_off')
 if not isinstance(cause, str) or cause == '':
-    raise Exception('data not valid')
+    raise Exception('invalid argument "data"')
 
 # Get Target JSON
 fileDir = os.path.dirname(os.path.abspath(__file__))
