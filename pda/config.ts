9bcda1887e291f96086951a8545feaaf4e6438ac
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-01-14 12:07:30 -0300

    adding more descriptors

--- pda/config.ts
+++ pda/config.ts
@@ -19,7 +19,12 @@ type Link = {
 type Signal = {
     Name: string
     DataType: number
-    Unit: string
     Active: boolean
-    Expression: string
+    Unit?: string
+    Expression?: string
+    FileSignalId?: string
 }
+
+const tagRegex = /([0-9]+)((.|:){1})([0-9]+)/
+const tagExample = '14:4'
+const tagNameExample = 'STD15 M_ACT'
